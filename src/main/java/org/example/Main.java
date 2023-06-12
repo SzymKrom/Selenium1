6ce57c98957cec82d812d4cb370488fb888ed03c
SzymKrom <szymonkromiszewski@gmail,com>
2023-06-12 20:05:34 +0200

    dodanie logowania

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\szymo\\OneDrive\\Pulpit\\Repozytoria\\Selenium\\chromedriver\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.w3schools.com/xml/xpath_syntax.asp");
-        driver.findElement(By.xpath("//*[@id=\"accept-choices\"]")).click();
+        driver.get("https://demoqa.com/books");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("login")).click();
+        driver.findElement(By.id("userName")).sendKeys("gunjankaushik");
+        driver.findElement(By.id("password")).sendKeys("Password@123");
+        driver.findElement(By.id("login")).click();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Witamy na stronie:" + driver.getCurrentUrl());
+        driver.quit();
     }
 }
 
