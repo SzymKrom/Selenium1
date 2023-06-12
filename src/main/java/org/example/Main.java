8ed1ef47b6ee44347ca65ea2de3ce05d96eccf04
SzymKrom <szymonkromiszewski@gmail,com>
2023-06-12 19:08:03 +0200

    dodanie opcji auto akceptacji cookies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\szymo\\OneDrive\\Pulpit\\Repozytoria\\Selenium\\chromedriver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.w3schools.com/xml/xpath_syntax.asp");
+        driver.findElement(By.xpath("//*[@id=\"accept-choices\"]")).click();
     }
 }
+
+//*[@id="accept-choices"]
